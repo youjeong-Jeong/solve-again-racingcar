a1f0d2c1914e9326d0450b853df0fbd50fc046b9
youjeong Jeong <wjddbwjd1205@naver.com>
2023-12-14 22:54:41 +0900

    feat: 각 자동차 별로 전진할지 말지 결정, 시도 횟수만큼 결정된 이동을 진행, 최종적으로 누가 우승했는지 확인

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -1,12 +1,60 @@
-import {ERROR} from "./Constants.js";
+import { Random } from "@woowacourse/mission-utils";
+import { ERROR } from "./Constants.js";
 import InputView from "./InputView.js";
 
 class RacingGame {
-  constructor() {}
+  #moveResult;
+
+  constructor() {
+    this.#moveResult = [];
+  }
 
   async start() {
     const carNames = await this.userInputCarName();
     const tryCount = await this.userInputTryCount();
+    this.#moveResult = this.resetMoveResult(carNames);
+    this.repeatGame(carNames, tryCount);
+    const winner = this.checkWinner(carNames);
+
+  }
+
+  checkWinner(carNames = []) {
+    const moveCountList = this.getLengths();
+    const maxNum = Math.max(...moveCountList);
+    const winner = [];
+    this.#moveResult.forEach( (result, idx) => {
+      if(result.length === maxNum) {
+        winner.push(carNames[idx]);
+      }
+    })
+    return winner;
+  }
+
+  getLengths() {
+    return this.#moveResult.map(element => element.length);
+  }
+
+  resetMoveResult(carNames = []) {
+    const resetArray = [];
+    carNames.forEach( () => {
+      resetArray.push([]);
+    })
+    return resetArray;
+  }
+
+  repeatGame(carNames = [], tryCount = 0) {
+    for(let i = 0; i < tryCount; i += 1)  {
+      carNames.forEach( (car, idx) => {
+        this.decisionToMove(idx);
+      })
+    }
+  }
+
+  decisionToMove(idx = 0) {
+    const result = Random.pickNumberInRange(0, 9);
+    if(result >= 4) {
+      this.#moveResult[idx].push('-');
+    }
   }
 
   async userInputTryCount() {
@@ -23,7 +71,7 @@ class RacingGame {
 
   validateOutOfRange(inputTryCount = '') {
     const tryCount = Number(inputTryCount);
-    if(tryCount < 1) {
+    if (tryCount < 1) {
       throw new Error(ERROR.error);
     }
   }
@@ -52,15 +100,15 @@ class RacingGame {
   }
 
   validateCarNameLength(carNames = []) {
-    carNames.forEach( name => {
-      if(name.length > 5) {
+    carNames.forEach(name => {
+      if (name.length > 5) {
         throw new Error(ERROR.error);
       }
     })
   }
 
   validateOneCar(inputCarNames = '') {
-    if(!inputCarNames.includes(',')) {
+    if (!inputCarNames.includes(',')) {
       throw new Error(ERROR.error);
     }
   }
