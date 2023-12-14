329b64ea88f6b5d6f459e16fe68134e2a17f7122
youjeong Jeong <wjddbwjd1205@naver.com>
2023-12-14 22:30:58 +0900

    feat: 시도할 횟수 입력 받기

--- src/RacingGame.js
+++ src/RacingGame.js
@@ -6,6 +6,36 @@ class RacingGame {
 
   async start() {
     const carNames = await this.userInputCarName();
+    const tryCount = await this.userInputTryCount();
+  }
+
+  async userInputTryCount() {
+    const inputTryCount = await InputView.readTryCount();
+    this.validateTryCount(inputTryCount);
+    return Number(inputTryCount);
+  }
+
+  validateTryCount(inputTryCount = '') {
+    this.validateIsEmpty(inputTryCount);
+    this.validateInteger(inputTryCount);
+    this.validateOutOfRange(inputTryCount);
+  }
+
+  validateOutOfRange(inputTryCount = '') {
+    const tryCount = Number(inputTryCount);
+    if(tryCount < 1) {
+      throw new Error(ERROR.error);
+    }
+  }
+
+  validateInteger(inputTryCount = '') {
+    const inputAsNumber = Number(inputTryCount);
+    if (Number.isNaN(Number(inputAsNumber))) {
+      throw new Error(ERROR.error);
+    }
+    if (!Number.isInteger(inputAsNumber)) {
+      throw new Error(ERROR.error);
+    }
   }
 
   async userInputCarName() {
